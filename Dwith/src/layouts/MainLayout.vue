53d9b204e6c009eaac5159eba286035d9d937db3
Cadu-Menezes <cadumenezes051@gmail.com>
2022-09-29 15:14:48 -0300

    Comecei

---
+++ Dwith/src/layouts/MainLayout.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    
+    <q-header elevated class="topo">
+      <q-toolbar>
+    
+        <q-toolbar-title id="Titulo">
+          Gamaria Adv 
+        </q-toolbar-title>
+
+        <q-btn :ripple="false" color="black" label="No ripple" no-caps class="Btn-Menu"/>
+        <q-btn :ripple="{ color: 'yellow' }" color="black" label="Yellow ripple" no-caps class="Btn-Menu"/>
+        <q-btn :ripple="{ center: true }" color="black" label="Center ripple" no-caps class="Btn-Menu"/>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+  
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+
+export default defineComponent({
+  name: 'MainLayout',
+
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
+
+sty/<style lang="scss">
+.Btn-Menu{
+margin-left: 20px;
+}
+
+.topo{
+  background-color: #F0FFFF;
+}
+
+#Titulo{
+  color: black;
+}
+</style>
---
+++ Dwith/src/pages/IndexPage.vue
@@ -0,0 +1,83 @@
+<template>
+<div class="Principal">
+  
+  <div class="BackgroundImage">
+    <div >
+      <q-parallax src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <h1 class="text-white">Gamaria Advogados</h1>
+      </q-parallax>
+    </div>
+  </div>
+
+  <h1 id="TituloAtuacao">Áreas de atuação</h1>
+
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md" >
+      <q-tabs v-model="tab" dense class="bg-grey-2 text-teal">
+        <q-tab name="mails" icon="mail" label="Mails"></q-tab>
+        <q-tab name="alarms" icon="alarm" label="Alarms" />
+        <q-tab name="movies" icon="movie" label="Movies" />
+      </q-tabs>
+    </div>
+  </div>
+
+
+
+
+</div>
+</template>
+
+
+<script>
+  import { defineComponent } from 'vue'
+  import { ref } from 'vue'
+
+  export default defineComponent({
+    name: 'IndexPage',
+   
+
+
+    setup() {
+    return {
+      tab: ref(''),
+    }
+  }
+  
+  })
+</script>
+
+
+
+
+<style scoped>
+  .Principal{
+    height: 100%;
+  }
+
+  .BackgroundImage{
+      display: block;
+      width: 100vw;
+      height: 100vh;
+      padding: 0;
+      text-align: center;
+      color: white;
+      background: url(https://cdn.quasar.dev/img/parallax2.jpg) no-repeat bottom center scroll;
+      background-color: black;
+      -webkit-background-size: contain;
+      -moz-background-size: contain;
+      background-size: contain;
+      -o-background-size: contain;
+  }
+
+  .q-pa-md{
+    width: 100%;
+  }
+
+  #TituloAtuacao{
+      
+      display: flex;
+      text-align: center;
+      margin-left: 300px;
+  }
+
+</style>
